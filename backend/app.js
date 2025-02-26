6ea20a356936bba1cf4061dcbff61e290d0c0de8
Ivan <ivancastropablo@hotmail.com>
2025-02-26 15:45:02 +0100

    vite installed, backend developed

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,35 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const path = require("path");
-const logging = require("./routers/logging")
-const calls = require("./routers/middlewares")
+const logging = require("./routers/logging");
 
-app.set("views", path.join(__dirname, "views"));
+// uso cors porque la aplicación usa dos puertos distintos
+app.use(cors({
+    credentials: true
+}));
 
+// middlewares para las llamadas a la api
+const calls = require("./routers/middlewares");
+
+// un middleware para todas las llamadas que da información en consola.
 app.use(logging);
 
 app.use("/", calls);
 
-// para servir react...
-app.use(express.static(path.join(__dirname, "../frontend/ASDF")))
-
+// sirviendo react usando vite...
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/ASDF"))
-})
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+const url = "http://localhost:3000/dashboard"
+
+app.listen(PORT,() => {
+    console.log(`Servidor corriendo en el puerto ${PORT}\n\n-----------------------\n| VISITE \x1B]8;;${url}\x1B\\ESTE ENLACE.\x1B]8;;\x1B\\ |\n-----------------------\n`);
+});
+
+module.exports = {
+    app
+}; 
--- backend/routers/middlewares.js
+++ backend/routers/middlewares.js
@@ -3,8 +3,3 @@ const router = express.Router();
 const { Movies } = require("../bd/models/Movies");
 const path = require("path");
 require('dotenv').config({path: "../.env"});
-
-// Endpoint: Login estático
-router.get("/login", (req, res) => {
-    res.render("login"); // Renderiza login.ejs
-});
